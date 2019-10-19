117ae261bd78124e2f61a0d02f9bfd42f02d561e
joshuarabanal <joshuarabanal@gmail.com>
2019-10-19 05:57:27 +0100

    from raspberry pi_PR

--- upload_toGithub.sh
+++ upload_toGithub.sh
@@ -2,14 +2,12 @@ cd "/home/pi/Documents/github/VPN"
 sudo git config --global credential.helper.cache
 sudo git config --global user.email "joshuarabanal@gmail.com"
 sudo git config --global user.name "joshuarabanal"
-echo "insert your commit message for VPN"
-read message
 echo "fetching for any changes"
 sudo git pull "https://joshuarabanal:SU0798ni@github.com/joshuarabanal/VPN.git"
 echo "adding any new files"
 sudo git add -A
-echo "uploading changes $(date, '+%Y-%m-%d')"
-sudo git commit -m "$message" -a
+echo "uploading changes "
+sudo git commit -m "from raspberry pi_PR" -a
 
 sudo git push "https://joshuarabanal:SU0798ni@github.com/joshuarabanal/VPN.git"
 
