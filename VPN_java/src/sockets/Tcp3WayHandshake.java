3e12ddb7a1325eab73000c45a2c2ce2659743dfb
joshuarabanal <joshuarabanal@gmail.com>
2019-11-06 21:16:50 +0000

    from raspberry pi_PR

---
+++ VPN_java/src/sockets/Tcp3WayHandshake.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sockets;
+
+/**
+ *
+ * @author root
+ */
+public class Tcp3WayHandshake {
+    /**
+     * 
+     * @param synRequest the recieved syn request
+     * @return the response [syn+ack] request
+     */
+    public static byte[] sendStep2(byte[] synRequest ){
+        
+    }
+    
+}
